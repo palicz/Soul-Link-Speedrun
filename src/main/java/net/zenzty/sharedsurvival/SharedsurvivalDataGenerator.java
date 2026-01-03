5b55a10bdc5d3d33e1e990d1b195d55867b726f6
palicz <hdtoki@gmail.com>
2026-01-03 18:53:57 +0100

    Initial commit

---
+++ src/main/java/net/zenzty/sharedsurvival/SharedsurvivalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zenzty.sharedsurvival;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SharedsurvivalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
