dbf877d30781beb695363949d8ef04b532cd84d1
zenzty <hdtoki@gmail.com>
2026-06-18 23:06:02 +0200

    chore: remove unused empty datagen scaffolding

--- src/main/java/net/zenzty/soullink/SharedsurvivalDataGenerator.java
+++
@@ -1,9 +0,0 @@
-package net.zenzty.soullink;
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-
-public class SharedsurvivalDataGenerator implements DataGeneratorEntrypoint {
-    @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
-}
